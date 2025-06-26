61bd1c1657b236a0cbd7da152aa79fad47e6a931
Tommy <ihelpmactutorials@gmail.com>
2025-06-27 09:19:52 +1000

    fixed checkout issue

--- amazon-one-click/main.py
+++ amazon-one-click/main.py
@@ -10,7 +10,7 @@ driver = webdriver.Chrome()
 
 login_email = ""
 login_password = ""
-item_asin = "B0F6Q92F5H"  # ASIN for the item to be purchased
+item_asin = "B07614PPV8"  # ASIN for the item to be purchased
 
 driver.get("https://www.amazon.com.au/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com.au%2F%3Fref_%3Dnav_custrec_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=auflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0")
 
@@ -36,31 +36,23 @@ try:
 except Exception as e:
     print("Error entering password:", e)
 
-driver.get("https://www.amazon.com.au/gp/product/handle-buy-box/ref=dp_start-bbf_1_glance?ASIN=" +  item_asin + "&quantity=1&submit.buy-now=Submit&account-linking=yes&dropdown-selection=add-new&dropdown-selection-ubb=add-new&tag=&offeringID=VtIBMis9dIKLXRNN5wUVdhjvy58X3npmke1E7AqLID1E2cxrIiuyRyc9puD473jjNdpCj1chNpF6oG%2FKIj8uAfag2DWUefDbg4SPL%2FB266AhP%2BwsXXr0w%2Fbtthu0kOtfsdBKZwP%2FS0l5IAynqTS0dq9mHkmEkpMr")
+driver.get("https://www.amazon.com.au/gp/product/handle-buy-box/ref=dp_start-bbf_1_glance?ASIN=" +  item_asin + "&quantity=1&submit.buy-now=Submit&account-linking=yes&dropdown-selection=add-new&dropdown-selection-ubb=add-new&tag=&offeringID=")
 
 # Wait for the title to be exactly "Place Your Order", refreshing if not
 max_retries = 1000000
 for attempt in range(max_retries):
     try:
-        WebDriverWait(driver, 2).until(
-            EC.title_is("Place Your Order")
+        place_order_btn = WebDriverWait(driver, 2).until(
+        EC.element_to_be_clickable((By.XPATH, "//*[@id='bottomSubmitOrderButtonId']/span/input"))
         )
-        print("Navigated to the checkout page successfully.")
+        place_order_btn.click()
+        print("Clicked 'Place your order' button successfully.")
         break
     except Exception:
         print(f"Title not matched, refreshing... (attempt {attempt + 1})")
         driver.refresh()
 else:
     print("Failed to reach the 'Place Your Order' page after several attempts.")
-
-try:
-    place_order_btn = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//*[@id='bottomSubmitOrderButtonId']/span/input"))
-    )
-    place_order_btn.click()
-    print("Clicked 'Place your order' button successfully.")
-except Exception as e:
-    print("Error clicking 'Place your order' button:", e)
 
 print("Order placed successfully.")
 
